dbf4e7c07aa987c13fef2a840d6ca4152004ba44
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-10-31 22:09:52 -0600

    testing prints

--- fb_bot/views.py
+++ fb_bot/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views import generic
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.views.generic import View
 from django.http.response import HttpResponse
+import json
 # Create your views here.
-class BotView(generic.View):
-    def get(self, request, *args, **kwargs):
+class BotView(View):
+    '''def get(self, request, *args, **kwargs):
 	if self.request.GET['hub.verify_token'] == 'v4l1d4710n70k3n':
             return HttpResponse(self.request.GET['hub.challenge'])
         else:
             return HttpResponse('Error, invalid token')
+    '''
+    # The get method is the same as before.. omitted here for brevity
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return View.dispatch(self, request, *args, **kwargs)
 
+    # Post function to handle Facebook messages
+    def post(self, request, *args, **kwargs):
+        # Converts the text payload into a python dictionary
+        incoming_message = json.loads(self.request.body.decode('utf-8'))
+        # Facebook recommends going through every entry since they might send
+        # multiple messages in a single call during high load
+        for entry in incoming_message['entry']:
+            for message in entry['messaging']:
+                # Check to make sure the received call is a message call
+                # This might be delivery, optin, postback for other events 
+                if 'message' in message:
+                    # Print the message to the terminal
+                    pprint(message)     
+        return HttpResponse()
