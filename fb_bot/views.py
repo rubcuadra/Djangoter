5cbe749daec5656035b493efd61ff97328387b88
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-11-01 13:00:55 -0600

    print token

--- fb_bot/views.py
+++ fb_bot/views.py
@@ -25,24 +25,15 @@ jokes = { 'stupid': ["""Yo' Mama is so stupid, she needs a recipe to make ice cu
 
 # Helper function
 def post_facebook_message(fbid, recevied_message):
+    
     # Remove all punctuations, lower case the text and split it based on space
     tokens = re.sub(r"[^a-zA-Z0-9\s]",' ',recevied_message).lower().split()
-    joke_text = ''
-    for token in tokens:
-        if token in jokes:
-            joke_text = random.choice(jokes[token])
-            break
-    if not joke_text:
-        joke_text = "I didn't understand! Send 'stupid', 'fat', 'dumb' for a Yo Mama joke!" 
+    
+    print tokens
 
-    user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid 
-    user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN} 
-    user_details = requests.get(user_details_url, user_details_params).json() 
-    joke_text = 'Yo '+user_details['first_name']+'..! ' + joke_text
-                   
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
     response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
-    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=tokens)
     pprint(status.json())
 
 # Create your views here.
