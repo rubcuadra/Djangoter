4484d2d9f3b4f6cd56f90255dcf3849768fcb89f
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-11-01 13:11:01 -0600

    amonos

--- fb_bot/views.py
+++ fb_bot/views.py
@@ -30,7 +30,7 @@ def post_facebook_message(fbid, recevied_message):
 
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
     response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":response}})
-    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=tokens)
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
     pprint(status.json())
 
 # Create your views here.
