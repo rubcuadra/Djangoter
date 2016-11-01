c04347ba803cff429983ac9558cadc7dbbaadb5b
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-10-31 22:45:39 -0600

    refactor

--- fb_bot/views.py
+++ fb_bot/views.py
@@ -1,29 +1,56 @@
-from django.shortcuts import render
+import json, requests, random, re
+from pprint import pprint
 
-# Create your views here.
+from django.views import generic
+from django.http.response import HttpResponse
+
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
-from django.views.generic import View
-from django.http.response import HttpResponse
-import json
-import os
 
-print os.environ
-token = os.getenv('token')
+#  ------------------------ Fill this with your page access token! -------------------------------
+PAGE_ACCESS_TOKEN = os.getenv('token')
+VERIFY_TOKEN = "v4l1d4710n70k3n"
 
-import requests
+jokes = { 'stupid': ["""Yo' Mama is so stupid, she needs a recipe to make ice cubes.""", 
+                     """Yo' Mama is so stupid, she thinks DNA is the National Dyslexics Association."""], 
+         'fat':      ["""Yo' Mama is so fat, when she goes to a restaurant, instead of a menu, she gets an estimate.""", 
+                      """ Yo' Mama is so fat, when the cops see her on a street corner, they yell, "Hey you guys, break it up!" """], 
+         'dumb': ["""Yo' Mama is so dumb, when God was giving out brains, she thought they were milkshakes and asked for extra thick.""", 
+                  """Yo' Mama is so dumb, she locked her keys inside her motorcycle."""] }
+
+# Helper function
+def post_facebook_message(fbid, recevied_message):
+    # Remove all punctuations, lower case the text and split it based on space
+    tokens = re.sub(r"[^a-zA-Z0-9\s]",' ',recevied_message).lower().split()
+    joke_text = ''
+    for token in tokens:
+        if token in jokes:
+            joke_text = random.choice(jokes[token])
+            break
+    if not joke_text:
+        joke_text = "I didn't understand! Send 'stupid', 'fat', 'dumb' for a Yo Mama joke!" 
+
+    user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid 
+    user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN} 
+    user_details = requests.get(user_details_url, user_details_params).json() 
+    joke_text = 'Yo '+user_details['first_name']+'..! ' + joke_text
+                   
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
+    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+    pprint(status.json())
+
 # Create your views here.
-class BotView(View):
+class YoMamaBotView(generic.View):
     def get(self, request, *args, **kwargs):
-	if self.request.GET['hub.verify_token'] == 'v4l1d4710n70k3n':
+        if self.request.GET['hub.verify_token'] == VERIFY_TOKEN:
             return HttpResponse(self.request.GET['hub.challenge'])
         else:
             return HttpResponse('Error, invalid token')
-            
-    # The get method is the same as before.. omitted here for brevity
+        
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
-        return View.dispatch(self, request, *args, **kwargs)
+        return generic.View.dispatch(self, request, *args, **kwargs)
 
     # Post function to handle Facebook messages
     def post(self, request, *args, **kwargs):
@@ -37,14 +64,8 @@ class BotView(View):
                 # This might be delivery, optin, postback for other events 
                 if 'message' in message:
                     # Print the message to the terminal
-                    print message     
-                    #Only working using plain text
-                    post_facebook_message(message['sender']['id'], message['message']['text'])
-        return HttpResponse()
-
-def post_facebook_message(fbid, recevied_message):           
-    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token='+token
-    print token
-    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":recevied_message}})
-    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
-    print status.json()
+                    pprint(message)    
+                    # Assuming the sender only sends text. Non-text messages like stickers, audio, pictures
+                    # are sent as attachments and must be handled accordingly. 
+                    post_facebook_message(message['sender']['id'], message['message']['text'])    
+        return HttpResponse()  
