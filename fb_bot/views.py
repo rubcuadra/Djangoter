ad0bb10e9cd65ad7422b3730ca29c19912a2fa2d
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-11-01 16:37:21 -0600

    enviorn

--- fb_bot/views.py
+++ fb_bot/views.py
@@ -7,6 +7,8 @@ from django.http.response import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
+print os.environ
+
 #  ------------------------ Fill this with your page access token! -------------------------------
 PAGE_ACCESS_TOKEN = os.getenv('token')
 VERIFY_TOKEN = "v4l1d4710n70k3n"
