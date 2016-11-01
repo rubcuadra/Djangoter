b7078ca2963ce931caa0b674a619562a38c89f41
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-10-31 22:21:38 -0600

    getenv and token

--- fb_bot/views.py
+++ fb_bot/views.py
@@ -5,7 +5,10 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.views.generic import View
 from django.http.response import HttpResponse
+import requests
 import json
+from os import getenv
+token = getenv('token')
 # Create your views here.
 class BotView(View):
     '''
@@ -34,4 +37,13 @@ class BotView(View):
                 if 'message' in message:
                     # Print the message to the terminal
                     print message     
+                    #Only working using plain text
+                    post_facebook_message(message['sender']['id'], message['message']['text'])
         return HttpResponse()
+
+def post_facebook_message(fbid, recevied_message):           
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token='+token
+    print token
+    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":recevied_message}})
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+    print status.json()
