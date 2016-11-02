b4f12d03e53079b2a1b5afc8bdb5197194181012
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-11-01 21:01:32 -0600

    notif type

--- fb_bot/views.py
+++ fb_bot/views.py
@@ -60,7 +60,7 @@ class BotView(generic.View):
                     elif message['message']['text']=='moonman':
                         video = "/home/ubuntu/botster/media/moonman.mp4"
                         print 'VIDEO ',message['sender']['id']
-                        bot.send_video(message['sender']['id'] ,video)
+                        bot.send_video(message['sender']['id'] ,video, notification_type='REGULAR')
                     
                     bot.send_text_message(message['sender']['id'], \
                                           message['message']['text'])
