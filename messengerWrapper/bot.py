17e31d0e55e9853380f0dccc074bbe0b0e3c0bad
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-11-01 22:31:54 -0600

    button class

--- messengerWrapper/bot.py
+++ messengerWrapper/bot.py
@@ -21,8 +21,17 @@ class Element(dict):
         return json.dumps({k:v for k, v in self.iteritems() if k in self.__acceptable_keys})
 
 class Button(dict):
-    # TODO: Decide if this should do more
-    pass
+    __acceptable_keys = ['type','url','title','payload']
+
+    def __init__(self, *args, **kwargs):
+        if six.PY2:
+            kwargs = {k:v for k, v in kwargs.iteritems() if k in self.__acceptable_keys}
+        else:
+            kwargs = {k:v for k, v in kwargs.items() if k in self.__acceptable_keys}
+        super(Element, self).__init__(*args, **kwargs)
+
+    def to_json(self):
+        return json.dumps({k:v for k, v in self.iteritems() if k in self.__acceptable_keys})
 
 
 class NotificationType(Enum):
