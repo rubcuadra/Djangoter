fd4f3fd302d6348d8fa380cfb92b5d91c01e589c
rubcuadra <ru_cuadra_e_96@hotmai.com>
2016-11-01 22:34:00 -0600

    super

--- messengerWrapper/bot.py
+++ messengerWrapper/bot.py
@@ -28,7 +28,7 @@ class Button(dict):
             kwargs = {k:v for k, v in kwargs.iteritems() if k in self.__acceptable_keys}
         else:
             kwargs = {k:v for k, v in kwargs.items() if k in self.__acceptable_keys}
-        super(Element, self).__init__(*args, **kwargs)
+        super(Button, self).__init__(*args, **kwargs)
 
     def to_json(self):
         return json.dumps({k:v for k, v in self.iteritems() if k in self.__acceptable_keys})
